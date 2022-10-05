6fe982ec0ec1004914707e95e0cb6b5de6628f4c
flicknewb <theowenmorris@gmail.com>
2022-10-05 14:37:11 -0700

    working Android build

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -27,6 +27,7 @@ import CardView from '../controls/CardView';
 import WalkerLogoComponent from '../assets/Walker_Logo_JSX';
 import QualtricsLogoComponent from '../assets/Qualtrics_logo_JSX';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {ScrollView} from 'react-native-gesture-handler';
 
 function WelcomeScreen({auth, setLogin, setCreds, setVars}) {
   const [isBusy, setIsBusy] = useState(false);
@@ -34,7 +35,6 @@ function WelcomeScreen({auth, setLogin, setCreds, setVars}) {
   const [projectID, setProjectID] = useState(auth.creds.projectID);
   const [extRefID, setExtRefID] = useState(auth.creds.extRefID);
   const [doExtRef, toggleExtRef] = useState(auth.creds.doExtRef);
-  const [keyboarAvoidEnable, toggleKeyboardAvoid] = useState(true);
   const navigation = useNavigation();
   const walkerURL = 'https://walkerinfo.com/resources/';
 
@@ -163,167 +163,163 @@ function WelcomeScreen({auth, setLogin, setCreds, setVars}) {
     toggleExtRef(!doExtRef);
   };
 
-  const disableKeyboardView = () => {
-    console.log('disable keyboard avoiding');
-    toggleKeyboardAvoid(false);
-  };
-
-  const enableKeyboardView = () => {
-    console.log('enable keyboard avoiding');
-    toggleKeyboardAvoid(true);
-  };
-
   const openWalkerHelp = () => {
     Linking.openURL(walkerURL);
   };
 
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <View
-        style={{
-          backgroundColor: '#417cca',
-          height: 50,
-          //backgroundColor: 'red',
-          position: 'absolute',
-          width: '110%',
-        }}></View>
-      <KeyboardAvoidingView
-        style={styles.keyboardAvoidingView}
-        enabled={keyboarAvoidEnable}
-        behavior="padding">
-        <Spinner visible={isBusy} textContent={''} />
+    <>
+      <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
         <TouchableOpacity
-          style={styles.touchableOpac}
-          onPress={() => {
-            Keyboard.dismiss();
-            disableKeyboardView();
-          }}
+          style={{backgroundColor: '#417cca'}}
+          onPress={Keyboard.dismiss}
           activeOpacity={1}>
-          <View style={styles.header}>
-            <WalkerLogoComponent
-              width="300"
-              height="53"
-              style={styles.walkerLogo}
-            />
-            <Text style={styles.subHeader}>Digital CX Mobile Demo</Text>
-          </View>
-          <View style={styles.body}>
-            <Text style={styles.inputHeader}>BrandID:</Text>
-            <TextInput
-              style={styles.textInput}
-              value={brandID}
-              placeholder="Brand ID"
-              placeholderTextColor="#adb5bd"
-              onChangeText={brandTextChange}
-              onPressIn={enableKeyboardView}
-              autoCapitalize="none"
-            />
-            <Text style={styles.inputHeader}>ProjectID:</Text>
-            <TextInput
-              style={styles.textInput}
-              value={projectID}
-              placeholder="Project ID"
-              placeholderTextColor="#adb5bd"
-              onChangeText={projectTextChange}
-              onPressIn={enableKeyboardView}
-              autoCapitalize="none"
-            />
-            <View style={styles.exRefContainer}>
-              <Switch
-                trackColor={{false: '#548ab4', true: '#81b2fc'}}
-                value={doExtRef}
-                onValueChange={doExtRefValChange}
-              />
-              <Text style={styles.exRefText}>
-                Initilize with External Data Reference
-              </Text>
-            </View>
-            {doExtRef ? (
-              <>
-                <Text style={styles.inputHeader}>External Reference ID:</Text>
-                <TextInput
-                  style={styles.textInput}
-                  value={extRefID}
-                  autoCapitalize="none"
-                  placeholder="External Reference ID"
-                  placholderTextColor="#adb5bd"
-                  onChangeText={extRefTextChange}
-                  onPressIn={enableKeyboardView}
-                />
-              </>
-            ) : (
-              <></>
-            )}
-            <TouchableOpacity style={styles.initBtn} onPress={initilizeQualt}>
-              <View style={styles.initBtnContent}>
-                <Text style={styles.initBtnTxt}>Load Project</Text>
-                <FontAwesomeIcon
-                  icon="arrow-right"
-                  size={22}
-                  style={styles.initBtnIcon}
-                />
-              </View>
-            </TouchableOpacity>
-          </View>
+          <WalkerLogoComponent width="300" height="53" style={styles.Wlogo} />
+          <Text style={styles.subHeader}>Digital CX Mobile Demo</Text>
         </TouchableOpacity>
-      </KeyboardAvoidingView>
-      <View style={styles.footer}>
+        <ScrollView style={{flexGrow: 0}} vertical={true}>
+          <TouchableOpacity onPress={Keyboard.dismiss} activeOpacity={1}>
+            <Spinner visible={isBusy} textContent={''} />
+            <KeyboardAvoidingView
+              style={{backgroundColor: '#417cca'}}
+              behavior="padding">
+              <CardView style={styles.card}>
+                <Text style={styles.inputTitleText}>Brand ID:</Text>
+                <TextInput
+                  style={styles.input}
+                  value={brandID}
+                  placeholder="Brand ID"
+                  placholderTextColor="#adb5bd"
+                  onChangeText={brandTextChange}
+                  autoCapitalize="none"
+                />
+                <Text style={styles.inputTitleText}>Project ID:</Text>
+                <TextInput
+                  style={styles.input}
+                  value={projectID}
+                  autoCapitalize="none"
+                  placeholder="Project ID"
+                  placholderTextColor="#adb5bd"
+                  onChangeText={projectTextChange}
+                />
+                <View style={styles.toggleInput}>
+                  <Switch
+                    trackColor={{false: '#548ab4', true: '#81b2fc'}}
+                    value={doExtRef}
+                    onValueChange={doExtRefValChange}
+                  />
+                  <Text style={{marginLeft: 15, fontFamily: my_font}}>
+                    Initilize with External Data Reference
+                  </Text>
+                </View>
+                {doExtRef ? (
+                  <>
+                    <Text style={styles.inputTitleText}>
+                      External Reference ID:
+                    </Text>
+                    <TextInput
+                      style={styles.input}
+                      value={extRefID}
+                      autoCapitalize="none"
+                      placeholder="External Reference ID"
+                      placholderTextColor="#adb5bd"
+                      onChangeText={extRefTextChange}
+                    />
+                  </>
+                ) : (
+                  <></>
+                )}
+                <TouchableOpacity
+                  style={styles.initBtn}
+                  onPress={initilizeQualt}>
+                  <View style={styles.initBtnContent}>
+                    <Text style={styles.initBtnTxt}>Load Project</Text>
+                    <FontAwesomeIcon
+                      icon="arrow-right"
+                      size={22}
+                      style={styles.initBtnIcon}
+                    />
+                  </View>
+                </TouchableOpacity>
+              </CardView>
+            </KeyboardAvoidingView>
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+      <CardView style={styles.footer}>
         <TouchableOpacity style={styles.helpContainer} onPress={openWalkerHelp}>
           <FontAwesomeIcon
+            style={{color: '#417cca'}}
             icon="question-circle"
-            size={20}
-            style={{color: '#417cca'}}></FontAwesomeIcon>
+            size={20}></FontAwesomeIcon>
           <Text style={styles.helpText}>Help</Text>
         </TouchableOpacity>
-        <QualtricsLogoComponent style={styles.qLogo} height="80" width="150" />
-      </View>
-    </SafeAreaView>
+        <QualtricsLogoComponent style={styles.Qlogo} height="50" width="150" />
+      </CardView>
+    </>
   );
 }
 
 const my_font = 'HelveticaNeue';
 
 const styles = StyleSheet.create({
-  safeAreaView: {
-    //flex: 1,
-    //backgroundColor: '#417cca',
+  logoContainer: {},
+  Wlogo: {
+    alignSelf: 'center',
+    alignContent: 'center',
+    marginTop: 10,
+    marginRight: 0,
   },
-  keyboardAvoidingView: {
-    flexDirection: 'column',
-    flex: 1,
-  },
-  touchableOpac: {
-    flex: 1,
-  },
-  header: {
-    backgroundColor: '#417cca',
-    alignItems: 'center',
-    flex: 1,
-  },
-  walkerLogo: {
-    marginVertical: 5,
+  Qlogo: {
+    alignSelf: 'center',
+    alignContent: 'center',
   },
   subHeader: {
+    alignSelf: 'center',
     marginBottom: 10,
     marginTop: 5,
     fontSize: 20,
     color: 'white',
     fontFamily: my_font,
   },
-  body: {
-    backgroundColor: 'white',
-    paddingBottom: 0,
-    flex: 5,
+  helpTextContainer: {
+    alignContent: 'center',
+    //marginVertical: 10,
+    //marginTop: 0,
   },
-  inputHeader: {
-    marginLeft: 18,
-    fontSize: 19,
-    fontWeight: 'normal',
-    marginTop: 5,
+  helpText: {
+    alignSelf: 'center',
+    fontSize: 20,
     fontFamily: my_font,
   },
-  textInput: {
-    marginTop: 10,
+  helpTextLink: {
+    alignSelf: 'center',
+    color: '#548ab4',
+    fontSize: 20,
+  },
+  card: {
+    marginTop: 15,
+    alignSelf: 'center',
+    width: '90%',
+    backgroundColor: '#ff00ff',
+    borderWidth: 1,
+    borderColor: '#e9ecef',
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: '#ffffff',
+  },
+  header: {
+    alignSelf: 'center',
+    fontSize: 20,
+    marginVertical: 20,
+    paddingBottom: 20,
+    margin: 5,
+    fontWeight: 'bold',
+    textShadowRadius: 0.5,
+    textShadowColor: 'black',
+  },
+  input: {
+    marginTop: 6,
     margin: 10,
     height: 45,
     borderWidth: 0.0,
@@ -333,22 +329,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#f4f8fb',
     fontFamily: my_font,
   },
-  exRefContainer: {
+  inputTitleText: {
+    marginLeft: 18,
+    fontSize: 19,
+    fontWeight: 'normal',
+    marginTop: 0,
+    fontFamily: my_font,
+  },
+  toggleInput: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 15,
-    marginTop: 17,
-  },
-  exRefText: {
-    marginLeft: 15,
-    fontFamily: my_font,
+    marginTop: 5,
   },
   initBtn: {
-    marginTop: 2,
+    marginTop: 5,
     marginHorizontal: 10,
-    marginTop: 30,
-    marginBottom: 1000,
+    marginBottom: 100,
     height: 50,
     fontSize: 5,
     backgroundColor: '#f7971e',
@@ -372,27 +370,27 @@ const styles = StyleSheet.create({
   initBtnIcon: {
     color: 'white',
   },
-  footer: {
-    backgroundColor: 'white',
-    alignItems: 'center',
-    width: '100%',
-    borderWidth: 1,
-    borderColor: '#e9ecef',
-    borderRadius: 10,
-  },
   helpContainer: {
     flexDirection: 'row',
     justifyContent: 'center',
-    alignContent: 'center',
-    paddingTop: 10,
+    alignItems: 'center',
+    marginRight: 5,
+    marginTop: -8,
   },
   helpText: {
-    fontSize: 20,
+    fontSize: 15,
     marginLeft: 10,
     color: '#417cca',
   },
-  qLogo: {
-    marginTop: -10,
+  footer: {
+    alignSelf: 'center',
+    alignContent: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    flex: 1,
+  },
+  Qlogo: {
+    alignSelf: 'center',
   },
 });
 
