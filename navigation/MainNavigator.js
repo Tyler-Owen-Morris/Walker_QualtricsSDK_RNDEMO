31a05d5982c8ab650ae781f66a911c422450c3f0
Morris <tmorris@walkerinfo.com>
2023-08-19 12:54:22 -0700

    initial commit of stable background task functioning for Android

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -1,15 +1,69 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {LogBox, StatusBar} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, { useEffect, useState, useRef } from 'react';
+import { connect } from 'react-redux';
+import { AppState, LogBox, StatusBar } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import FirstIntercept from '../screens/FirstIntercept';
+import BackgroundService from 'react-native-background-actions';
 
 LogBox.ignoreAllLogs();
 
 const Stack = createStackNavigator();
 
-function MainNavigator({auth}) {
+const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
+const backgroundTask = async (taskArguments) => {
+  const { delay } = taskArguments;
+  await new Promise(async (resolve) => {
+    for (let i = 0; BackgroundService.isRunning(); i++) {
+      console.log("bg:", i);
+      await sleep(delay);
+    }
+  })
+}
+const options = {
+  taskName: 'BG Checker',
+  taskTitle: 'BG Checker',
+  taskDesc: 'ExampleTask description',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+  parameters: {
+    delay: 1000,
+  },
+};
+
+function MainNavigator({ auth }) {
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        console.log('App has come to the foreground!');
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log('AppState', appState.current);
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (appStateVisible == 'background') {
+      BackgroundService.start(backgroundTask, options);
+    } else {
+      BackgroundService.stop();
+    }
+  }, [appStateVisible])
   //console.log("Auth:", auth.auth);
   let bgcolor = '#417cca';
   return (
@@ -25,7 +79,7 @@ function MainNavigator({auth}) {
             <Stack.Screen
               name="WelcomeScreen"
               component={WelcomeScreen}
-              options={{headerShown: false}}
+              options={{ headerShown: false }}
             />
           </>
         ) : (
@@ -33,7 +87,7 @@ function MainNavigator({auth}) {
             <Stack.Screen
               name="Welcome"
               component={FirstIntercept}
-              options={{headerShown: false}}
+              options={{ headerShown: false }}
             />
           </>
         )}
@@ -41,7 +95,7 @@ function MainNavigator({auth}) {
     </>
   );
 }
-const mapStateToProps = ({auth}) => ({
+const mapStateToProps = ({ auth }) => ({
   auth,
 });
 
